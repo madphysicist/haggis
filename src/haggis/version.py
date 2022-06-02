5dbff8c64f0a0dca2c7e09dc35bae1fc1bd348e5
Joseph R. Fox-Rabinovitz <jfoxrabinovitz@gmail.com>
2022-06-02 11:48:21 -0500

    MAINT: Bumped version to 0.5.0rc1

--- src/haggis/version.py
+++ src/haggis/version.py
@@ -30,6 +30,7 @@
 # Version: 29 Mar 2022: Bumped to 0.4.0rc1
 # Version: 29 Mar 2022: Opened to 0.3.1 backport branch
 # Version: 02 Jun 2022: Bumped to 0.4.0
+# Version: 02 Jun 2022: Bumped to 0.5.0rc1
 
 
 """
@@ -52,4 +53,4 @@ back in.
 
 
 #: The current version
-__version__ = '0.4.0'
+__version__ = '0.5.0rc1'
