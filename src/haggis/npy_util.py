4792d7f73f975960b962bcf6964809c687b55aa1
Joseph R. Fox-Rabinovitz <jfoxrabinovitz@gmail.com>
2022-10-17 09:23:01 -0500

    ENH: Added npy_util.masked_index, unmasked_index, find_peaks

--- src/haggis/npy_util.py
+++ src/haggis/npy_util.py
@@ -22,6 +22,7 @@
 # Version: 27 Jan 2022: Moved mask2runs, runs2mask from math
 # Version: 28 Mar 2022: Added iterate_dtype
 # Version: 01 Jun 2022: Added replace_field
+# Version: 15 Oct 2022: Added masked_index, unmasked_index, find_peaks
 
 
 """
@@ -39,8 +40,8 @@ from .numbers import digit_count
 
 
 __all__ = [
-    'isolate_dtype', 'iterate_dtype', 'map_array', 'mask2runs',
-    'replace_field', 'runs2mask'
+    'find_peaks', 'isolate_dtype', 'iterate_dtype', 'map_array', 'mask2runs',
+    'masked_index', 'replace_field', 'runs2mask', 'unmasked_index'
 ]
 
 
@@ -443,3 +444,246 @@ def runs2mask(runs, n=None):
 
     numpy.cumsum(view, out=view)
     return mask
+
+
+def unmasked_index(index, mask):
+    """
+    Convert `index` in a masked aray into the corresponding index in the
+    original.
+
+    Given an array ``x`` of the same size as `mask`, determine the
+    position of ``x[mask][index]`` in the original array ``x``. Since
+    `mask` may be multi-dimensional but ``x[mask]`` is always raveled,
+    the result may be a scalar or a tuple.
+
+    Parameters
+    ----------
+    index : array-like[int]
+        The index or indices into in the masked (raveled) array. Each
+        element must be in
+        ``[-np.count_nonzero(mask), np.count_nonzero(mask))``.
+    mask : array-like[bool]
+        A boolean mask to determine the indices of the array.
+        Non-boolean mask arrays will be interpreted as booleans, as
+        though with ``mask.astype(bool)``. Scalar arrays will be
+        interpreted as one-element arrays, so scalar indexing 
+        allowed.
+
+    Return
+    ------
+    unmasked_index : int or numpy.ndarray or tuple
+        If `mask` is a scalar or 1D, the result is the same size and
+        shape as `index`. Otherwise, it is a tuple of length
+        ``mask.ndim`` each of whose elements corresponds to a dimension,
+        and it the same size and shape as `index``.
+    """
+    index = numpy.asanyarray(index)
+    mask = numpy.asanyarray(mask)
+
+    if index.size == 0:
+        empty = numpy.array([], dtype=numpy.intp)
+        return (empty,) * mask.ndim if mask.ndim > 1 else empty
+    indices = numpy.argwhere(mask)
+    if mask.ndim > 1:
+        return tuple(indices.T[:, index])
+    indices = indices[index].ravel()
+    return indices if index.ndim else indices.item()
+
+
+def masked_index(index, mask):
+    """
+    Convert N-dimensional `index` into its corresponding location in the
+    masked array, if it is in the masked portion.
+
+    Given an array ``x``, of the same size as `mask`, determine the
+    position of ``x[index]`` in ``x[mask]``. If `index` is not in the
+    masked portion, the corressponding output is set to ``-1``.
+
+    Parameters
+    ----------
+    index : tuple
+        The index or indices into the unmasked multi-dimensional array.
+        A non-tuple (scalar or array) index may be used for 1D masks,
+        will be wrapped in a tuple internally to avoid ambiguity. The
+        number of elements in the tuple must match ``mask.ndim``, and
+        all elements must all broadcast together.
+    mask : array-like[bool]
+        A boolean mask to determine the indices of the array.
+        Non-boolean mask arrays will be interpreted as booleans, as
+        though with ``mask.astype(bool)``. Scalar arrays will be
+        interpreted as one-element arrays, so scalar indexing 
+        allowed.
+
+    Return
+    ------
+    masked_index : int or None
+        If `index` corresponds to a `True` element of `mask`, return its
+        position in the masked result. Otherwise, return `None`.
+    """
+    mask = numpy.asanyarray(mask, dtype=numpy.bool_)
+    if not isinstance(index, tuple):
+        index = (index,)
+    if mask.ndim > 0 and mask.size > 0:
+        if mask.ndim != len(index):
+            raise ValueError('index must have length {}'.format(mask.ndim))
+        # The following two lines are a hack until a new mode is added. See
+        # https://github.com/numpy/numpy/issues/15475#issuecomment-1280056626
+        index = tuple(numpy.asanyarray(i) for i in index)
+        index = tuple(numpy.where(i < 0, i + s, i)
+                            for i, s in zip(index, mask.shape))
+        try:
+            index = numpy.ravel_multi_index(index, mask.shape, mode='raise')
+        except ValueError as e:
+            raise IndexError(str(e))
+    mask = mask.ravel()
+    indices = mask.cumsum(dtype=numpy.intp)
+    indices -= 1
+    return numpy.where(mask[index], indices[index], numpy.intp(-1))
+
+
+def find_peaks(arr, n_peaks=None, mode='value', return_values=False):
+    """
+    Find the locations of the `n_peaks` tallest or leftmost (unfiltered)
+    local maxima of `arr`.
+
+    When ``mode=='value'``, the first peak is located at
+    ``arr.argmax()``. The second peak is the maximum value among the
+    numbers that are not monotonically non-increasing away from the
+    first peak. Successive peaks are returned from remaining portions
+    of the array.
+
+    For a plateau with multiple equal peak elements, only the first is
+    returned. Similarly, if multiple peaks with the same values are
+    found, they will be retreived in order of increasing index.
+
+    Parameters
+    ----------
+    arr : array_like
+        The data to search through. Expected to be 1D. Larger dimensions
+        are raveled to avoid the problem of different numbers of results
+        along a given axis.
+    n_peaks : int or None
+        The number of peaks to search for. None or negative searches for
+        all available peaks. If `n_peaks` local maxima are not found,
+        the result will be shorter than requested.
+    mode : str
+        One of {``'value'``, ``'index'``} (case insensitive):
+
+        - ``'value'``: Return up to the first `n_peaks` tallest peaks,
+          regardless of location.
+        - ``'index'``: Return up to the first `n_peaks` local maxima,
+          traversing the array from left to right, regardless of peak
+          height.
+    return_values : bool
+        Return a second array containing the maximum values at each
+        location. Convenience for ``arr[find_peaks(arr, ...)]``.
+
+    Return
+    ------
+    peak_indices : numpy.ndarray[int]
+        Locations of up to `n_peaks` local maxima. May be shorter than
+        `n_peaks` if there are insufficient local maxima in the data.
+    peak_values : numpy.ndarray
+        Elements of `arr` at `peak_indices`.
+
+    Notes
+    -----
+    To get peaks in reverse order with ``mode='index'``, reverse the
+    input: ``arr[::-1]``.
+
+    To get peaks sorted from left to right with ``mode='value'``, sort
+    the results.
+    """
+    arr = numpy.asanyarray(arr).ravel()
+    n = len(arr)
+    if n_peaks is None:
+        n_peaks = -1
+
+    if not isinstance(mode, str):
+        raise TypeError('mode must be one of the strings {{valid, index}}, '
+                        'not {}'.format(type(mode).__name__))
+    cmode = mode.casefold()
+
+    indices = []
+    if cmode == 'value':
+        mask = numpy.ones(arr.shape, dtype=numpy.bool_)
+        while n and len(indices) != n_peaks:
+            index = unmasked_index(arr[mask].argmax(), mask)
+            indices.append(index)
+            # Find surrounding window
+            start = mask[index::-1].argmin() # First zero before
+            if start != 0:
+                start = index - (start - 1)
+            end = mask[index:].argmin()      # First zero after
+            if end == 0:
+                end = len(arr)
+            else:
+                end += index
+            index -= start
+            diff = numpy.diff(arr[start:end])
+            zero = diff.dtype.type(0)
+            # Find left side start of peak
+            if index > 1:
+                sstart = (diff[index - 1::-1] < zero).argmax()
+                # If index is zero, entire left side is non-increasing
+                if sstart != 0:
+                    # Otherwise, adjust for reversal
+                    sstart = index - sstart
+            else:
+                sstart = 0
+            # Find right side end of peak
+            if index < end - start - 2:
+                send = (diff[index:] > zero).argmax()
+                # If index is zero, entire right side is non-increasing
+                if send != 0:
+                    end = start + index + send + 1
+            start += sstart
+            mask[start:end] = False
+            n -= end - start
+    elif cmode == 'index':
+        subset = arr
+        offset = 0
+        while n and len(indices) != n_peaks:
+            if n == 1:
+                index = 0
+                end = 1
+            else:
+                diff = numpy.diff(subset)
+                zero = diff.dtype.type(0)
+                end = (diff < zero).argmax()
+                if diff[end] >= 0:
+                    # end == 0, since subset is constantly rising, set to last
+                    end = n
+                    index = n - 1
+                else:
+                    # Find beginning of plateau
+                    index = (diff[end::-1] > zero).argmax()
+                    # If index is zero, entire subset is plateau, leave it at 0
+                    if index != 0:
+                        # Otherwise, adjust for reversal
+                        index = end - (index - 1)
+            # Found a peak
+            indices.append(index + offset)
+            # Find end of trough
+            if end < n - 2:
+                end += (diff[end + 1:] > zero).argmax() + 1
+                if diff[end] <= 0:
+                    # Trough never ends
+                    end = n
+                else:
+                    # Select first element of uptrend
+                    end += 1
+            else:
+                end = n
+            # Update subset
+            offset += end
+            n -= end
+            subset = subset[end:]
+    else:
+        raise ValueError('mode must be one of {{valid, index}}, '
+                         'not {}'.format(mode))
+
+    indices = numpy.array(indices, dtype=numpy.intp)
+    if return_values:
+        return indices, arr[indices]
+    return indices
--- src/haggis/tests/test_npy_util.py
+++ src/haggis/tests/test_npy_util.py
@@ -19,6 +19,7 @@
 
 # Author: Joseph Fox-Rabinovitz <jfoxrabinovitz at gmail dot com>
 # Version: 01 Jun 2022: Initial Coding
+# Version: 15 Oct 2022: Tests for masked_index, unmasked_index, find_peaks
 
 
 """
@@ -29,7 +30,7 @@ Tests for the :py:mod:`haggis.npy_util` module.
 import numpy as np
 from pytest import raises
 
-from ..npy_util import replace_field
+from ..npy_util import find_peaks, masked_index, replace_field, unmasked_index
 
 
 class TestReplaceField:
@@ -133,3 +134,255 @@ class TestReplaceField:
 
     def test_missing(self):
         raises(ValueError, replace_field, self.inner, np.float32, 'XXX')
+
+
+class TestUnmaskedIndex:
+    def test_empty_array(self):
+        assert (unmasked_index((), []) == []).all()
+        assert (unmasked_index([], []) == []).all()
+        raises(IndexError, unmasked_index, 0, [])
+
+    def test_empty_mask(self):
+        raises(IndexError, unmasked_index, 0, [False, False, False])
+
+    def test_bad_type(self):
+        raises(IndexError, unmasked_index, [1.0, 2.0], np.ones(5, bool))
+
+    def test_non_bool(self):
+        expected = [1, 3]
+        mask = [1.0, 2.0, 0.0, 3.0]
+        result = unmasked_index([1, 2], mask)
+        assert (result == expected).all()
+
+    def test_out_of_bounds(self):
+        raises(IndexError, unmasked_index, 2, [True, True, False])
+        raises(IndexError, unmasked_index, -3, [True, True, False])
+
+    def test_tuple(self):
+        expected = [4, 3]
+        result = unmasked_index((3, 2), [True, True, False, True, True])
+        assert (result == expected).all()
+
+    def test_f_order(self):
+        expected = ([0, 1], [1, 2])
+        index = [1, 2]
+        mask = np.array([[True, True, False], [False, False, True]], order='F')
+
+        result = unmasked_index(index, mask)
+
+        assert len(result) == 2
+        for r, e in zip(result, expected):
+            assert (r == e).all()
+
+    def test_simple(self):
+        expected = [4, 0, 1, 7]
+        index = [2, 0, 1, 3]
+        mask = [True, True, False, False, True, False, False, True]
+        result = unmasked_index(index, mask)
+        assert (result == expected).all()
+
+    def test_ndarray(self):
+        expected = ([[2, 0], [1, 0]], [[2, 1], [3, 0]])
+        index = [[4, 1], [3, 0]]
+        mask = [[True, True, False, False],
+                [True, False, False, True],
+                [False, False, True, False]]
+        result = unmasked_index(index, mask)
+        assert len(result) == 2
+        for r, e in zip(result, expected):
+            print(r, e, sep='\n')
+            assert (r == e).all()
+
+    def test_negative(self):
+        expected = ([[2, 0], [1, 0]], [[2, 1], [3, 0]])
+        index = [[-1, -4], [-2, -5]]
+        mask = [[True, True, False, False],
+                [True, False, False, True],
+                [False, False, True, False]]
+        result = unmasked_index(index, mask)
+        assert len(result) == 2
+        for r, e in zip(result, expected):
+            assert (r == e).all()
+
+
+class TestMaskedIndex:
+    def test_out_of_bounds(self):
+        raises(IndexError, masked_index, 0, [])
+        raises(IndexError, masked_index, -3, [True, False])
+        raises(IndexError, masked_index, 2, [True, False])
+        raises(IndexError, masked_index, (-1, 2), [[True, False]])
+        raises(IndexError, masked_index, (1, 0), [[True, False]])
+
+    def test_wrong_dims(self):
+        raises(ValueError, masked_index, 0, [[True]])
+        raises(ValueError, masked_index, (0, 0, 0), [[True]])
+
+    def test_promotion(self):
+        result = masked_index(0, False)
+        assert result == -1
+
+        raises(IndexError, masked_index, 1, False)
+
+    def test_not_bool(self):
+        result = masked_index(0, [1, 2])
+        assert result == 0
+
+    def test_unmasked_portion(self):
+        result = masked_index((), False)
+        assert result == -1
+
+        result = masked_index(0, [False, True])
+        assert result == -1
+
+        result = masked_index((1, 1), [[True, True], [True, False]])
+        assert result == -1
+
+    def test_simple(self):
+        expected = [0, 2, 3]
+        index = [1, 3, 4]
+        mask = [False, True, True, True, True, False]
+        result = masked_index(index, mask)
+        assert (result == expected).all()
+
+    def test_ndarray(self):
+        expected = [[0, 8, 1, 2], [9, 11, 3, 7]]
+        index = (
+            [[0, 1, -2, 0], [-1, 1, -2, -1]],
+            [[0, -2, -3, 1], [-2, 2, 1, 0]],
+            [[0, -4, 1, -3], [2, -2, 3, -1]]
+        )
+        mask = [
+            [[True, True, False, False],
+             [False, True, False, True],
+             [False, False, True, True]],
+            [[True, False, False, True],
+             [True, False, True, False],
+             [False, True, True, False]]
+        ]
+        result = masked_index(index, mask)
+        assert (result == expected).all()
+
+    def test_negative(self):
+        expected = [0, -1]
+        index = ([1, 0], [0, 0])
+        mask = [[False], [True]]
+        result = masked_index(index, mask)
+        assert (result == expected).all()
+
+    def test_broadcast(self):
+        expected = [
+            [[[0, -1, 1], [0, -1, 1]],
+             [[-1, 2, 3], [-1, 2, 3]],
+             [[0, -1, 1], [0, -1, 1]]],
+            [[[-1, 2, 3], [-1, 2, 3]],
+             [[0, -1, 1], [0, -1, 1]],
+             [[-1, 2, 3], [-1, 2, 3]]]
+        ]
+        index = (
+            [[[[0]], [[1]], [[0]]], [[[1]], [[0]], [[1]]]],
+            [[0, 1, 2], [0, 1, 2]]
+        )
+        mask = [[True, False, True], [False, True, True]]
+        result = masked_index(index, mask)
+        assert result.shape == (2, 3, 2, 3)
+        assert (result == expected).all(None)
+
+
+class TestFindPeaks:
+    def test_empty(self):
+        result = find_peaks([])
+        assert (result == []).all()
+
+    def test_single(self):
+        result = find_peaks([1.0])
+        assert (result == [0]).all()
+
+    def test_flat(self):
+        result = find_peaks([0, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 3, 3, 3, 3, 3])
+        assert (result == [11, 1]).all()
+
+    def test_order_n(self):
+        result = find_peaks([0, 2, 1, 3], n_peaks=1)
+        assert (result == [3]).all()
+
+        result = find_peaks([0, 2, 1, 3], n_peaks=5)
+        assert (result == [3, 1]).all()
+
+    def test_dates(self):
+        expected_ind = [3, 5]
+        expected_val = np.array(['2022-10-14 10:00:00', '2022-10-14 00:00:00'],
+                                dtype='datetime64[ns]')
+        dates = np.array([
+            '2022-10-13 10:00:00', '2022-10-13 10:30:00',
+            '2022-10-13 11:00:00', '2022-10-14 10:00:00',
+            '2022-10-13 23:00:00', '2022-10-14 00:00:00'],
+            dtype='datetime64[ns]')
+        ind, val = find_peaks(dates, mode='value', return_values=True)
+        print(ind, expected_ind, sep='\n')
+        assert (ind == expected_ind).all()
+        assert val.dtype == dates.dtype
+        assert (val == expected_val).all()
+
+        ind, val = find_peaks(dates, mode='index', return_values=True)
+        assert (ind == expected_ind).all()
+        assert val.dtype == dates.dtype
+        assert (val == expected_val).all()
+
+    def test_obj(self):
+        data = np.empty(5, dtype=object)
+        data[0] = []
+        data[1] = set()
+        data[2] = np.zeros(3)
+        data[3] = {}
+        data[4] = '1'
+        raises(TypeError, find_peaks, data)
+
+    def test_mode(self):
+        data = [1, 1, 0, 2, 4, 4, 4, 0, 3, 5]
+
+        raises(ValueError, find_peaks, data, mode='invalid')
+        raises(TypeError, find_peaks, data, mode=None)
+        raises(TypeError, find_peaks, data, mode=b'value')
+
+        expected = [9, 4, 0]
+        result = find_peaks(data, n_peaks=2, mode='value')
+        assert (result == expected[:2]).all()
+        result = find_peaks(data, mode='vAluE')
+        assert (result == expected).all()
+
+        expected = [0, 4, 9]
+        result = find_peaks(data, n_peaks=2, mode='index')
+        assert (result == expected[:2]).all()
+        result = find_peaks(data, mode='INdeX')
+        assert (result == expected).all()
+
+    def test_ravel(self):
+        data = [[0, 0, 0, 1], [1, 2, 3, 2], [1, 0, 0, 0]]
+        expected = [6]
+        result = find_peaks(data)
+        assert (result == expected).all()
+
+    def test_return_values(self):
+        result = find_peaks([0, 3, 0, 1], 2, return_values=True)
+        assert len(result) == 2
+
+        ind, val = result
+        assert (ind == [1, 3]).all()
+        assert (val == [3, 1]).all()
+
+    def test_npeaks(self):
+        data = [2, 0, 1, 3, 2, 4, 10, 8, 5, 6, 5, 4, 5]
+
+        expected = [6, 9, 12, 3, 0]
+        result = find_peaks(data, mode='value')
+        print(result)
+        print(expected)
+        assert (result == expected).all()
+        result = find_peaks(data, 2, mode='value')
+        assert (result == expected[:2]).all()
+
+        expected = [0, 3, 6, 9, 12]
+        result = find_peaks(data, mode='index')
+        assert (result == expected).all()
+        result = find_peaks(data, 3, mode='index')
+        assert (result == expected[:3]).all()
